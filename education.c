b3c360c010bc7d131c2142065b57cee1d762fbd4
siyasanga98 <siyasangavilakati98@gmail.com>
2023-04-24 14:11:38 -0700

    education.c

--- education.c
+++ education.c
@@ -1,48 +1,106 @@
 #include "main.h"
 
+/************************* PRINT CHAR *************************/
+
+/**
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
 int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+        int flags, int width, int precision, int size)
 {
-	char c = va_arg(types, int);
-	return (write(1, &c, 1));
+    // Retrieve a character from the variable arguments list
+    char c = va_arg(types, int);
+
+    // Call a helper function to handle writing the character to the buffer and return the number of characters printed
+    return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
+/************************* PRINT A STRING *************************/
+
+/**
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
 int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+        int flags, int width, int precision, int size)
 {
-	char *str = va_arg(types, char *);
-	if (str == NULL)
-		str = "(null)";
-	return (write(1, str, strlen(str)));
+    // Retrieve a string from the variable arguments list
+    char *str = va_arg(types, char *);
+
+    // If the string is NULL, replace it with "(null)" and adjust the precision
+    if (str == NULL)
+    {
+        str = "(null)";
+        if (precision >= 6)
+            str = "      ";
+    }
+
+    // Get the length of the string
+    int length = strlen(str);
+
+    // Adjust the length based on the precision
+    if (precision >= 0 && precision < length)
+        length = precision;
+
+    // If the width is greater than the length, add padding based on the flags
+    if (width > length)
+    {
+        if (flags & F_MINUS)
+        {
+            // Left-align the string and add padding on the right
+            write(1, &str[0], length);
+            for (int i = width - length; i > 0; i--)
+                write(1, " ", 1);
+            return (width);
+        }
+        else
+        {
+            // Right-align the string and add padding on the left
+            for (int i = width - length; i > 0; i--)
+                write(1, " ", 1);
+            write(1, &str[0], length);
+            return (width);
+        }
+    }
+
+    // If the width is less than or equal to the length, just write the string
+    return (write(1, str, length));
 }
 
+/************************* PRINT PERCENT SIGN *************************/
+
+/**
+ * print_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
 int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+        int flags, int width, int precision, int size)
 {
-	UNUSED(types);
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-	return (write(1, "%", 1));
-}
-
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	int n = va_arg(types, int);
-	char num_str[20];
-	sprintf(num_str, "%d", n);
-	return (write(1, num_str, strlen(num_str)));
-}
-
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	unsigned int n = va_arg(types, unsigned int);
-	char bin_str[33];
-	itoa(n, bin_str, 2);
-	return (write(1, bin_str, strlen(bin_str)));
-}
+    // Print the percent sign and return the number of characters printed
+    UNUSED(types);
+    UNUSED(buffer);
+    UNUSED(flags);
+    UNUSED(width);
+    UNUSED(precision);
+    UNUSED(size);
+    return (write(
 
