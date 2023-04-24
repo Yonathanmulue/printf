9728a61c37e8b84a541ebda32f42e6d0ceb25212
siyasanga98 <siyasangavilakati98@gmail.com>
2023-04-24 10:01:24 -0700

    _printf>c

--- main.h
+++ main.h
@@ -68,26 +68,26 @@ int print_reverse(va_list types, char buffer[],
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-
 /* width handler */
-int handle_write char(char c char buffer[],
-		int flag ,int width , int precision, int size);
-int write_number(int is_positive ,int ind , char buffer [],
-		int flag , int width , int precision , int size);
-int write_num(int ind ,char bff[], int flags, int width , int precision,
-		int lenght , char padd , char extra_c);
-int write_pointer(char buffer[] , int ind, int length ,
-		int width , int flags, char paddd , char extra_c, int padd_start);
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
 
-int write_unsigned(int is_negative, int ind,
+int write_unsgnd(int is_negative, int ind,
 char buffer[],
-		int flags, int width , int precision, int size);
+	int flags, int width, int precision, int size);
+
 /****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
 
-long int convert_size_number(long int  num, int size);
-long int convert_unsgnd(unsigned long int num, int size);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
