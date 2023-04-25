1c17973190d7b84ec586a740b456cd750c277d11
siyasanga98 <siyasangavilakati98@gmail.com>
2023-04-25 08:19:51 -0700

    changes

--- education.c
+++ education.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-/************************* PRINT CHAR *************************/
 
 /**
  * print_char - Prints a char
@@ -15,14 +14,11 @@
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
-    // Retrieve a character from the variable arguments list
     char c = va_arg(types, int);
 
-    // Call a helper function to handle writing the character to the buffer and return the number of characters printed
     return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
-/************************* PRINT A STRING *************************/
 
 /**
  * print_string - Prints a string
@@ -37,10 +33,8 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
         int flags, int width, int precision, int size)
 {
-    // Retrieve a string from the variable arguments list
     char *str = va_arg(types, char *);
 
-    // If the string is NULL, replace it with "(null)" and adjust the precision
     if (str == NULL)
     {
         str = "(null)";
@@ -48,19 +42,15 @@ int print_string(va_list types, char buffer[],
             str = "      ";
     }
 
-    // Get the length of the string
     int length = strlen(str);
 
-    // Adjust the length based on the precision
     if (precision >= 0 && precision < length)
         length = precision;
 
-    // If the width is greater than the length, add padding based on the flags
     if (width > length)
     {
         if (flags & F_MINUS)
         {
-            // Left-align the string and add padding on the right
             write(1, &str[0], length);
             for (int i = width - length; i > 0; i--)
                 write(1, " ", 1);
@@ -68,7 +58,6 @@ int print_string(va_list types, char buffer[],
         }
         else
         {
-            // Right-align the string and add padding on the left
             for (int i = width - length; i > 0; i--)
                 write(1, " ", 1);
             write(1, &str[0], length);
@@ -76,11 +65,9 @@ int print_string(va_list types, char buffer[],
         }
     }
 
-    // If the width is less than or equal to the length, just write the string
     return (write(1, str, length));
 }
 
-/************************* PRINT PERCENT SIGN *************************/
 
 /**
  * print_percent - Prints a percent sign
@@ -95,7 +82,6 @@ int print_string(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
         int flags, int width, int precision, int size)
 {
-    // Print the percent sign and return the number of characters printed
     UNUSED(types);
     UNUSED(buffer);
     UNUSED(flags);
---
+++ tests/temp2/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+void print_int(int n);
+
+
+#endif
